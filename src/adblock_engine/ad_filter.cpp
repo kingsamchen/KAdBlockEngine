c0134fbac2bda0dd7934278c4f846fd856486ecf
Kingsley Chen <kingsamchen@gmail.com>
2016-05-01 12:19:36 +0800

    AdFilter: Read filter rules in during class constructor.
    Also load filter info from comments during this construction.

--- src/adblock_engine/ad_filter.cpp
+++ src/adblock_engine/ad_filter.cpp
@@ -4,8 +4,77 @@
 
 #include "adblock_engine/ad_filter.h"
 
+#include "kbase/error_exception_util.h"
+#include "kbase/file_util.h"
+#include "kbase/string_util.h"
+#include "kbase/tokenizer.h"
+
+namespace {
+
+const char kVersionTagName[] = "Version";
+const char kTitleTagName[] = "Title";
+const char kLastModifiedTagName[] = "Last Modified";
+
+bool IsComment(kbase::StringView line)
+{
+    return kbase::StartsWith(line, "!") || kbase::StartsWith(line, "[");
+}
+
+bool IsExceptionRule(kbase::StringView line)
+{
+    return kbase::StartsWith(line, "@@");
+}
+
+}   // namespace
+
 namespace abe {
 
-// TODO: class implementation
+AdFilter::AdFilter(const kbase::Path& filter_file_path)
+{
+    std::string filter_data = kbase::ReadFileToString(filter_file_path);
+    // TODO: throw our own exception type.
+    ENSURE(RAISE, !filter_data.empty()).Require();
+    kbase::Tokenizer data_lines(filter_data, "\r\n");
+    for (auto&& line : data_lines) {
+        if (line.empty()) {
+            continue;
+        }
+
+        if (IsComment(line)) {
+            LoadFilterInfo(line);
+            continue;
+        }
+
+        if (IsExceptionRule(line)) {
+
+        } else {
+
+        }
+
+        // TODO: add to rule list.
+    }
+}
+
+const AdFilter::Info& AdFilter::GetFilterInfo() const
+{
+    return info_;
+}
+
+void AdFilter::LoadFilterInfo(kbase::StringView comment)
+{
+    auto colon_pos = comment.find(':');
+    if (colon_pos == kbase::StringView::npos) {
+        return;
+    }
+
+    auto info_tag = comment.substr(2, colon_pos - 2);
+    if (info_tag == kVersionTagName) {
+        info_.version = comment.substr(colon_pos + 2).ToString();
+    } else if (info_tag == kTitleTagName) {
+        info_.title = comment.substr(colon_pos + 2).ToString();
+    } else if (info_tag == kLastModifiedTagName) {
+        info_.last_modified = comment.substr(colon_pos + 2).ToString();
+    }
+}
 
 }   // namespace abe
--- src/adblock_engine/ad_filter.h
+++ src/adblock_engine/ad_filter.h
@@ -9,11 +9,33 @@
 #ifndef KADBLOCKENGINE_ADBLOCK_ENGINE_AD_FILTER_H_
 #define KADBLOCKENGINE_ADBLOCK_ENGINE_AD_FILTER_H_
 
+#include "kbase/basic_macros.h"
+#include "kbase/path.h"
+#include "kbase/string_view.h"
+
 namespace abe {
 
 class AdFilter {
 public:
+    struct Info {
+        std::string version;
+        std::string title;
+        std::string last_modified;
+    };
+
+    explicit AdFilter(const kbase::Path& filter_file_path);
+
     ~AdFilter() = default;
+
+    DISALLOW_COPY(AdFilter);
+
+    const Info& GetFilterInfo() const;
+
+private:
+    void LoadFilterInfo(kbase::StringView comment);
+
+private:
+    Info info_;
 };
 
 }   // namespace abe
